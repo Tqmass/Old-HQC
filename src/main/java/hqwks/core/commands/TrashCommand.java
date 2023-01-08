d581e9b1861093949ff679ef717054c9fe7349c2
Hqwksss <54636540+Hqwkss@users.noreply.github.com>
2023-01-08 11:20:49 +0100

    Update TrashCommand.java

--- src/main/java/hqwks/core/commands/TrashCommand.java
+++ src/main/java/hqwks/core/commands/TrashCommand.java
@@ -1,23 +1,17 @@
 package hqwks.core.commands;
 
-import hqwks.core.CoreMain;
 import hqwks.core.utils.Utilities;
 import org.bukkit.Bukkit;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
+import hqwks.core.utils.command.Command;
+import hqwks.core.utils.command.CommandInfo;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-public class TrashCommand implements CommandExecutor {
-
-    private CoreMain plugin;
-
-    public TrashCommand(CoreMain plugin) {
-        this.plugin = plugin;
-    }
+@CommandInfo(name = "trash", aliases = {"basurero"}, playerOnly = true)
+public class TrashCommand extends Command {
 
     @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+    public boolean onCommand(CommandSender sender, String[] args) {
         Player p = (Player) sender;
         p.sendMessage(Utilities.toColor("&8&l「&a&l✔&8&l」&b¡Has abierto el basurero!"));
         p.openInventory(Bukkit.createInventory(null, 9, Utilities.toColor("&e&lϟ &4&lBasurero &e&lϟ")));
